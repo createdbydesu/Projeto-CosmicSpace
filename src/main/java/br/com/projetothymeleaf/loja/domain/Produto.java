c317952021abbd82e1742e185611f948513f4f1f
createdbydesu <marcosguilherme.vicente@gmail.com>
2021-05-21 21:54:20 -0300

    Criando nossa tabela e a conexão com o banco de dados

---
+++ src/main/java/br/com/projetothymeleaf/loja/domain/Produto.java
@@ -0,0 +1,68 @@
+package br.com.projetothymeleaf.loja.domain;
+
+import br.com.projetothymeleaf.loja.to.ProdutoTO;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "PRODUTO")
+public class Produto {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "DATA_CRIACAO")
+    private Date dataCriacao = new Date();
+
+    @Column(name = "VALOR")
+    private String valor;
+
+    public Produto() {
+    }
+
+    public Produto(ProdutoTO produtoTO) {
+        this.id = produtoTO.getId();
+        this.nome = produtoTO.getNome();
+        this.valor = produtoTO.getValor();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+
+}
